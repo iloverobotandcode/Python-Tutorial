39a281d8016ed82c371f451eb086ca902cbc2d12
iloverobotandcode <142725879+iloverobotandcode@users.noreply.github.com>
2025-06-27 05:50:55 +0700

    Update Pro_1.py

--- Small_Projects/Project_1/Pro_1.py
+++ Small_Projects/Project_1/Pro_1.py
@@ -1,1 +1,8 @@
+// Project 1: Making an auto-generate set of multiplication table 
+print ("\nMULTIPLICATION TABLE")
 
+for i in range (2,10):
+    for j in range (1,11):
+        product = i*j
+        print ("| {}*{} = {}|".format(i,j, product))
+    print("\n")
